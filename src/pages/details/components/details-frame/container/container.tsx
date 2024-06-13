cc30f20d8fe9a3c7c545b1b0403e82160d352e01
Sergey <sergey.sav.26666@mail.ru>
2024-06-13 21:13:21 +0300

    PP-01 добавлены фреймы с листами"

--- src/pages/details/components/details-frame/container/container.tsx
+++ src/pages/details/components/details-frame/container/container.tsx
@@ -2,11 +2,13 @@ import { FrameTitle } from './frame-title/frame-title';
 import { FrameText } from './frame-text/frame-text';
 import { FrameList } from './frame-list/frame-list';
 import { ContainerView } from './container.view';
+import { FrameMultiList } from './frame-multi-list/frame-multi-list';
 
 type TDetailsFrameFragment = typeof ContainerView & {
   Title: typeof FrameTitle;
   Text: typeof FrameText;
   List: typeof FrameList;
+  MultiList: typeof FrameMultiList;
 };
 
 export const FrameFragment = ContainerView as TDetailsFrameFragment;
@@ -14,3 +16,4 @@ export const FrameFragment = ContainerView as TDetailsFrameFragment;
 FrameFragment.Title = FrameTitle;
 FrameFragment.Text = FrameText;
 FrameFragment.List = FrameList;
+FrameFragment.MultiList = FrameMultiList;
--- src/pages/details/components/details-frame/container/frame-multi-list/frame-multi-list.tsx
+++ src/pages/details/components/details-frame/container/frame-multi-list/frame-multi-list.tsx
@@ -2,6 +2,32 @@ import React from 'react';
 
 import styles from './frame-multi-list.module.scss';
 
-export const FrameMultiList: React.FC = () => {
-  return <div></div>;
+import { ListInfoBlock } from '@components/shared/blocks/list-info-block';
+import { getUniqueKey } from '@data/utils/core';
+import { ETextSize, ETextTheme, Text } from '@components/shared';
+
+interface IData {
+  url?: string;
+  text: string;
+}
+interface IProp {
+  data: Array<IData>;
+  isImg?: boolean;
+}
+
+export const FrameMultiList: React.FC<IProp> = ({ data, isImg }) => {
+  const renderList = () => {
+    return data.map((item, i) => (
+      <ListInfoBlock key={getUniqueKey(item.text, i)}>
+        {item.url && <img src={item.url} className={styles.img} alt='Тематическая иконка' />}
+        {
+          <Text size={ETextSize.SM} theme={ETextTheme.BLACK}>
+            {item.text}
+          </Text>
+        }
+      </ListInfoBlock>
+    ));
+  };
+
+  return <div className={isImg ? styles.imgGrid : styles.grid}>{renderList()}</div>;
 };
