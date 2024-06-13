c289e2b9be1d887ace275d6b80050ce5df3de583
Jakers <ilyaC303.spawn@outlook.com>
2024-06-13 14:31:05 +0300

    autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  },
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src/') },
