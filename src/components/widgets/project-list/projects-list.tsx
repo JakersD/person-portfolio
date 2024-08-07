1edbc77e606a4f758126852fa7b6cd18ec6b4f2f
Jakers <ilyaC303.spawn@outlook.com>
2024-08-07 23:51:49 +0300

    исправление пагинации списка проектов

--- src/components/widgets/project-list/projects-list.tsx
+++ src/components/widgets/project-list/projects-list.tsx
@@ -30,6 +30,7 @@ export const ProjectList: React.FC = () => {
     },
   };
 
+  const projects = getProjectsExcludeId(getProjectList(), getQueryFromUrl(url));
   return (
     <Wrapper>
       <Title className={styles.title} size={ETitleSize.H2}>
@@ -43,14 +44,16 @@ export const ProjectList: React.FC = () => {
         pagination={paginationConfig}
         grabCursor
       >
-        {getProjectsExcludeId(getProjectList(), getQueryFromUrl(url)).map((project, i) => (
+        {projects.map((project, i) => (
           <SwiperSlide key={getUniqueKey(project.id, i)}>
             <ProjectCard project={project} index={i} onClick={handleClickProject} />
           </SwiperSlide>
         ))}
-        <SwiperButtons>
-          <div className={styles.dots} />
-        </SwiperButtons>
+        {projects.length > 2 && (
+          <SwiperButtons>
+            <div className={styles.dots} />
+          </SwiperButtons>
+        )}
       </Swiper>
     </Wrapper>
   );
